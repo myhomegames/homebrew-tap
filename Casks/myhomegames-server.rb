b56d3515f722392d94342ecb33e9f861b85bea5d
Luca Stancapiano <luca.stancapiano@sourcesense.com>
2026-07-21 23:47:43 +0200

    chore(cask): myhomegames-server 1.2.0

--- Casks/myhomegames-server.rb
+++ Casks/myhomegames-server.rb
@@ -3,10 +3,10 @@ cask "myhomegames-server" do
   version "1.2.0"
 
   if Hardware::CPU.arm?
-    sha256 "0bd51e21915540db4623f582db5c02a812aae6c3e44bedf8ec974856c5d4938a"
+    sha256 "14e4b0a74d41d63a394a3c4f15e16cc8df2f7385d50155f1c08071cb5119d676"
     url "https://github.com/myhomegames/myhomegames-server/releases/download/1.2.0/MyHomeGames-1.2.0-mac-arm64.pkg"
   else
-    sha256 "12f9c73c30134c90e7463eee695b6d8b82e04651a3b0876da141bf6c8f4685d7"
+    sha256 "3627fadf7962d2f9be2161b3d24c10bfc5eed2f96f4fea8b95244b334eb38d35"
     url "https://github.com/myhomegames/myhomegames-server/releases/download/1.2.0/MyHomeGames-1.2.0-mac-x64.pkg"
   end
 
